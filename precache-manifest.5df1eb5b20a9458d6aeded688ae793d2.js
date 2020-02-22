93a7c9ee25d633c05a18705d479f1aee276778ef
rawfour <rafaldziedziela@outlook.com>
2020-02-22 18:41:38 +0100

    Updates

---
+++ precache-manifest.5df1eb5b20a9458d6aeded688ae793d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ede1cd2173e2185c50c7aec302fadcae",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "cfe928ad0ab454fb9b3e",
+    "url": "/weather-app-react/static/css/main.734dc181.chunk.css"
+  },
+  {
+    "revision": "543fae73a1761cac0626",
+    "url": "/weather-app-react/static/js/2.3a396725.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/weather-app-react/static/js/2.3a396725.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cfe928ad0ab454fb9b3e",
+    "url": "/weather-app-react/static/js/main.e9124e03.chunk.js"
+  },
+  {
+    "revision": "d9885b805ae5faf1bd40",
+    "url": "/weather-app-react/static/js/runtime-main.cdab807e.js"
+  },
+  {
+    "revision": "246c1201edbfbe27c6efc421dbc7eec5",
+    "url": "/weather-app-react/static/media/2850815.246c1201.jpg"
+  }
+]);
